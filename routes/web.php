2811bda28d1cc43af41c9caa30fef4fd13cabab5
Samuel Scozzari <s_scozzari@libero.it>
2022-03-03 16:07:41 +0100

    added array data

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    $array = config('comics');
+
+    return view('home', compact('array'));
 })->name('home');
